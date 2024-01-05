b432f7ef4cc167cca8b988aae1ae9154aef350bb
A540 <wlsdud2528@gmail.com>
2024-01-05 20:17:04 +0900

    db 설정 변경 -> mysql

--- Aone/settings.py
+++ Aone/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'Aone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '13.209.74.74',
+        'NAME': 'Aone',                         # 데이터베이스 스키마 이름
+        'USER': 'root',
+        'PASSWORD': 'ehxhfl1720',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
