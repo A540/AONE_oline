32be5a4d012f98d2a32ff77a1e945fd4467db4b3
A540 <wlsdud2528@gmail.com>
2024-01-15 18:00:19 +0900

    static 루트 설정

--- Aone/settings.py
+++ Aone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +50,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Aone.urls'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 TEMPLATES = [
     {
